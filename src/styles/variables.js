2cc7019bc3cc182fad632c5e9d48c026340165d8
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-07-25 22:30:57 +0800

    fix: order of projects

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -19,11 +19,11 @@ const variables = css`
     --font-sans: 'Calibre', 'San Francisco', 'SF Pro Text', -apple-system, system-ui, sans-serif;
     --font-mono: 'SF Mono', 'Fira Code', 'Fira Mono', 'Roboto Mono', monospace;
 
-    --fz-xxs: 12px;
-    --fz-xs: 13px;
-    --fz-sm: 14px;
-    --fz-md: 16px;
-    --fz-lg: 18px;
+    --fz-xxs: 14px;
+    --fz-xs: 15px;
+    --fz-sm: 16px;
+    --fz-md: 18px;
+    --fz-lg: 19px;
     --fz-xl: 20px;
     --fz-xxl: 22px;
     --fz-heading: 32px;
